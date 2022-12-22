a9e2ab86c6b22a467748293704a7448f3af85850
selvan7 <777tamil777@gmail.com>
2022-12-22 20:37:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cicdcomplete/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.cicdcomplete
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext," Success!!! ", Toast.LENGTH_LONG).show()
+
+    }
+
+}
